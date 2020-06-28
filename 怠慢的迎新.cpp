5fd2e96d111f672b0e85f6050be6ec52c239078b
Bdonsir <2209823792@qq.com>
2020-06-28 12:34:19 +0800

    emm,新增文件

--- 怠慢的迎新.cpp
+++ 怠慢的迎新.cpp
@@ -2,10 +2,6 @@
 #include <string>
 using namespace std;
 int main() {
-	string user_name;
 	cout << "Please enter your name: ";
-	cin >> user_name;
-	cout << endl;
-	cout << "hello," << user_name << " ... and goodbay!\n";
 	return 0;
 }
