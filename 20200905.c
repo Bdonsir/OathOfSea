20b87a955373029793260037db5013d04efae8c1
0xEthan <59382106+HeyEthan@users.noreply.github.com>
2021-01-11 14:38:36 +0800

    Delete 20200905.c

--- 20200905.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int main() {
-	int dogs = 0;
-	printf("How many dogs do you have?\n");
-	scanf("%d",&dogs);
-	printf("So you have %d dogs!", dogs);
-	return 0;
-}
